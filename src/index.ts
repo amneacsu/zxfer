85ead52408482560415c91075c14ae4f923f4e0f
Andrei Neacșu <0x514@protonmail.com>
2023-01-04 00:25:38 +0200

    fix: wait for user action

--- src/index.ts
+++ src/index.ts
@@ -3,50 +3,52 @@ import { AudioWorkletUrl } from 'audio-worklet';
 
 import { Oscilloscope } from './oscilloscope';
 
-const audioCtx = new AudioContext();
+document.getElementById('play').addEventListener('click', async () => {
+  const audioCtx = new AudioContext();
 
-await audioCtx.audioWorklet.addModule(
-  new AudioWorkletUrl(new URL('./decoder.ts', import.meta.url)) as string,
-);
+  await audioCtx.audioWorklet.addModule(
+    new AudioWorkletUrl(new URL('./decoder.ts', import.meta.url)) as string,
+  );
 
-const decoder = new AudioWorkletNode(audioCtx, 'decoder');
-// const audio = new Audio('Manic_Miner.wav');
-const audio = new Audio('1.wav');
-const source = audioCtx.createMediaElementSource(audio);
+  const decoder = new AudioWorkletNode(audioCtx, 'decoder');
+  // const audio = new Audio('Manic_Miner.wav');
 
-source.connect(decoder);
-// source.connect(audioCtx.destination);
+  const renderQuantums: Float32Array[] = [];
+  const edges: number[] = [];
 
-const renderQuantums: Float32Array[] = [];
-const edges: number[] = [];
+  // Draw render quantum
+  decoder.port.onmessage = (event) => {
+    const { type, payload } = event.data;
 
-// Draw render quantum
-decoder.port.onmessage = (event) => {
-  const { type, payload } = event.data;
-
-  switch (type) {
-    case 'quantum': {
-      renderQuantums.push(payload);
-      break;
+    switch (type) {
+      case 'quantum': {
+        renderQuantums.push(payload);
+        break;
+      }
+      case 'edge':
+        edges.push(payload);
+        break;
     }
-    case 'edge':
-      edges.push(payload);
-      break;
-  }
-};
+  };
 
-const viewer = new Oscilloscope();
+  const viewer = new Oscilloscope();
 
-const tick = () => {
-  viewer.clear();
-  viewer.drawGrid();
-  viewer.drawEdges(edges);
-  viewer.drawSamples(renderQuantums);
-};
+  const tick = () => {
+    viewer.clear();
+    viewer.drawGrid();
+    viewer.drawEdges(edges);
+    viewer.drawSamples(renderQuantums);
+  };
 
-// Wait for audio data
-audio.addEventListener('loadeddata', () => {
-  audio.currentTime = audio.duration / 2 + .33;
-  audio.play();
-  setInterval(tick, 200);
+  const audio = new Audio('1.wav');
+  const source = audioCtx.createMediaElementSource(audio);
+
+  source.connect(decoder);
+  // source.connect(audioCtx.destination);
+
+  audio.addEventListener('loadeddata', () => {
+    audio.currentTime = audio.duration / 2 + .33;
+    audio.play();
+    setInterval(tick, 200);
+  });
 });
