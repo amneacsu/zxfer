df79185d776f146543121957c9fd885ac9965d4e
Andrei Neacșu <0x514@protonmail.com>
2025-05-30 21:13:20 +0300

    bits

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,3 +1,5 @@
+import { DecoderState } from './types';
+
 const polarity = (value: number) => {
   const threshold = 0.01;
 
@@ -17,34 +19,113 @@ const isEdge = (() => {
   };
 })();
 
-class NoiseGenerator extends AudioWorkletProcessor {
+class Decoder extends AudioWorkletProcessor {
   index = 0;
+  polarity = -1;
+  count = 0;
+  state = DecoderState.WAITLEAD;
+  syncPulse1 = false;
+  syncPulse2 = false;
+  low = 0;
+  high = 0;
 
   constructor() {
     super();
+  }
+
+  setState(newState: DecoderState) {
+    this.state = newState;
+    this.port.postMessage({
+      type: 'newState',
+      payload: newState,
+    });
   }
 
   process(inputs: Float32Array[][]) {
     this.port.postMessage({ type: 'quantum', payload: inputs[0][0] });
     const input = inputs[0];
     const samples = input[0];
-    this.window1(samples);
-    // return this.index < 1024;
-    return true;
-  }
+    // console.log(samples.length);
 
-  last = 0;
-
-  window1(samples: Float32Array) {
     samples.forEach((sample, i) => {
+      const samplePolarity = polarity(sample);
+
+      if (samplePolarity !== this.polarity) {
+        if (this.polarity !== 0 && this.count > 0) {
+          if (this.count > 28) {
+            this.setState(DecoderState.LEAD);
+            this.syncPulse1 = false;
+            this.syncPulse2 = false;
+          }
+
+          switch (this.state) {
+            case DecoderState.WAITLEAD:
+              if (this.count > 28) {
+                this.setState(DecoderState.LEAD);
+                this.syncPulse1 = false;
+                this.syncPulse2 = false;
+              }
+              break;
+            case DecoderState.LEAD:
+              if (this.count < 20) {
+                if (!this.syncPulse1) {
+                  this.syncPulse1 = true;
+                  break;
+                }
+                if (!this.syncPulse2) {
+                  this.syncPulse2 = true;
+                  this.setState(DecoderState.PROG);
+                  break;
+                }
+                console.warn('Received 3rd sync pulse in lead phase');
+              }
+              break;
+            case DecoderState.PROG:
+              if (this.count < 15) {
+                this.low += 1;
+              } else {
+                this.high += 1;
+              }
+              if (this.low === 2 || this.high === 2) {
+                this.port.postMessage({
+                  type: 'bit',
+                  payload: this.low === 2 ? 0 : 1,
+                });
+                this.low = 0;
+                this.high = 0;
+              }
+              break;
+          }
+
+          this.port.postMessage({ type: 'polarity', payload: { count: this.count, polarity: this.polarity } });
+        }
+
+        // reset
+        this.polarity = samplePolarity;
+        this.count = 1;
+      } else {
+        this.count += 1;
+      }
+
       if (isEdge(sample)) {
-        this.last = this.index + i;
         this.port.postMessage({ type: 'edge', payload: this.index + i });
       }
     });
 
     this.index += samples.length;
+
+    // return this.index < 24096;
+    return true;
   }
 }
 
-registerProcessor('decoder', NoiseGenerator as any);
+registerProcessor('decoder', Decoder as any);
+
+/*
+
+12/24
+A '0' bit is encoded as 2 pulses of 855 T-states each.
+A '1' bit is encoded as 2 pulses of 1710 T-states each.
+x71.25
+44100 Hz
+*/
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import './style.css';
 import { AudioWorkletUrl } from 'audio-worklet';
 
 import { Oscilloscope } from './oscilloscope';
-import wav from './data/Jetpac.wav';
+import wav from './data/Manic_Miner.wav';
+import { DecoderState } from './types';
+// import wav from './data/Jetpac.wav';
 
 document.getElementById('play').addEventListener('click', async () => {
   const audioCtx = new AudioContext();
@@ -14,16 +16,29 @@ document.getElementById('play').addEventListener('click', async () => {
   const decoder = new AudioWorkletNode(audioCtx, 'decoder');
   // const audio = new Audio('Manic_Miner.wav');
 
+  const polarities: { count: number; polarity: -1 | 1 }[] = [];
   const renderQuantums: Float32Array[] = [];
   const edges: number[] = [];
+  let state: DecoderState = DecoderState.WAITLEAD;
+  const bits: number[] = [];
 
   // Draw render quantum
   decoder.port.onmessage = (event) => {
     const { type, payload } = event.data;
 
     switch (type) {
+      case 'newState':
+        state = payload;
+        break;
+      case 'bit':
+        bits.push(payload);
+        break;
       case 'quantum': {
         renderQuantums.push(payload);
+        break;
+      }
+      case 'polarity': {
+        polarities.push(payload);
         break;
       }
       case 'edge':
@@ -31,10 +46,24 @@ document.getElementById('play').addEventListener('click', async () => {
         break;
     }
 
+    const bytes = [];
+    for (let i = 0; i < bits.length / 8; i += 1) {
+      const x = bits.slice(i * 8, i * 8 + 8).join('');
+      bytes.push(parseInt(x, 2));
+    }
+
     document.getElementById('debug').innerHTML = `
 render quantums: ${renderQuantums.length}
+polarities: ${polarities.length}
 edges: ${edges.length}
+decoder state: ${state}
+bits: ${bits.join('')}
 `;
+
+    // console.log(edges.map((edge, index) => {
+    //   if (index === 0) return edge;
+    //   return edge - edges[index - 1];
+    // }));
   };
 
   const viewer = new Oscilloscope();
@@ -52,10 +81,10 @@ edges: ${edges.length}
   const source = audioCtx.createMediaElementSource(audio);
 
   source.connect(decoder);
-  // source.connect(audioCtx.destination);
+  source.connect(audioCtx.destination);
 
   audio.addEventListener('loadeddata', () => {
-    audio.currentTime = audio.duration / 2 + .33;
+    audio.currentTime = 4.9;
     audio.play();
     setInterval(tick, 1000 / 60);
   });
